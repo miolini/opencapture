8dc878570aa25670555b96440030eb8cb49430c8
miolini <miolini@miolini-laptop.(none)>
2012-02-19 23:40:46 -0500

    adding linux mock support

--- src/linux/linux.c
+++ src/linux/linux.c
@@ -1,5 +1,52 @@
 #include <opencapture.h>
 #include "linux.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct {
-} oc_linux_context
+oc_context_t* oc_context_create()
+{
+    oc_context_linux_t *context = (oc_context_linux_t*) malloc(sizeof(oc_context_linux_t));
+    return context;
+}
+
+void oc_context_destroy(oc_context_t *_context)
+{
+    oc_context_linux_t *context = (oc_context_linux_t*) _context;
+    free(context);
+}
+
+oc_device_list_t* oc_device_list_all(oc_context_t *_context, int type)
+{
+    oc_device_list_t *deviceList = malloc(sizeof(oc_device_list_t));
+    deviceList->count = 0;
+    return deviceList;
+}
+
+oc_device_list_t* oc_device_list(oc_context_t *_context)
+{
+    return oc_device_list_all(_context, 0);
+}
+
+oc_device_list_t* oc_device_list_video(oc_context_t *_context)
+{
+    return oc_device_list_all(_context, 1);
+}
+
+oc_device_list_t* oc_device_list_audio(oc_context_t *_context)
+{
+    return oc_device_list_all(_context, 2);
+}
+
+void oc_device_list_destroy(oc_device_list_t *devices)
+{
+    if (devices->count > 0)
+    {
+	for(int i=0;i<devices->count;i++)
+	{
+	    oc_device_t *device = devices->list[i];
+	    free(device);
+	}
+	free(devices->list);
+    }
+    free(devices);
+}
--- src/linux/linux.h
+++ src/linux/linux.h
@@ -1,4 +1,7 @@
 #ifndef OC_LINUX_H
 #define OC_LINUX_H
 
+typedef struct {
+} oc_context_linux_t;
+
 #endif
