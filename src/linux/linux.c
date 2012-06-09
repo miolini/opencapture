b0d285b66774e58d70b469f0c2ae7851568f60c2
den <den@debian.(none)>
2012-06-09 14:48:30 -0400

    The begining...

--- src/linux/linux.c
+++ src/linux/linux.c
@@ -1,7 +1,14 @@
-#include <opencapture.h>
-#include "linux.h"
+#include <opencapture.h> 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <string.h>
+#include <linux/videodev2.h>
+#include "linux.h"
 
 oc_context_t* oc_context_create()
 {
@@ -15,11 +22,53 @@ void oc_context_destroy(oc_context_t *_context)
     free(context);
 }
 
+int is_capture_device(int fd)
+{
+	int ret = 0;
+	struct v4l2_capability cap;
+	if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == 0)
+		if (cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)
+			ret = 1;
+
+	return ret;
+}
+
+int check_devices(const char *dev_pfx, oc_device_list_t *dev_list)
+{
+	int ret, i;
+	char devname[DEVNAME_LEN];
+	
+	for (i=0; i < MAXDEV; i++) {
+		snprintf(devname, DEVNAME_LEN-1, "%s%d", dev_pfx, i);
+		if ((ret = open(devname, O_RDWR)) > 0) {
+			if (is_capture_device(ret)) {
+				dev_list->count++;
+				dev_list->list = (oc_device_t **)realloc(dev_list->list,
+									(i+1)*sizeof(oc_device_t));
+				dev_list->list[i] =  malloc(sizeof(oc_device_t));
+				dev_list->list[i]->name = malloc(DEVNAME_LEN);
+				//or malloc(strlen(devname)+1)
+				memcpy(dev_list->list[i]->name, devname, DEVNAME_LEN);
+				//dev_list->list[i]->id?
+				//dev_list->list[i]->native?
+			}
+			close(ret);
+		} else return ret;
+	}
+	return ret;
+}
+
 oc_device_list_t* oc_device_list_by_type(oc_context_t *_context, int type)
 {
-    oc_device_list_t *deviceList = malloc(sizeof(oc_device_list_t));
-    deviceList->count = 0;
-    return deviceList;
+	oc_device_list_t *deviceList = malloc(sizeof(oc_device_list_t));
+
+	deviceList->count = 0;
+
+	check_devices(VIDEODEV_PFX, deviceList);
+	check_devices(AUDIODEV_PFX, deviceList);
+
+
+	return deviceList;
 }
 
 void oc_cli_monitor_video(const char *device_id)
@@ -41,3 +90,4 @@ void oc_device_list_destroy(oc_device_list_t *devices)
     }
     free(devices);
 }
+
--- src/linux/linux.h
+++ src/linux/linux.h
@@ -1,7 +1,13 @@
 #ifndef OC_LINUX_H
 #define OC_LINUX_H
 
+#define VIDEODEV_PFX    "/dev/video"
+#define AUDIODEV_PFX    "/dev/radio"
+#define DEVNAME_LEN     16
+#define MAXDEV		64      	//See ~linux/Documentation/devices.txt
+
 typedef struct {
 } oc_context_linux_t;
 
 #endif
+
